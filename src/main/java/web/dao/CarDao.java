3a902833f5293164013f30d5663b19d36aecb2cb
apenrose <aklinaev@gmail.com>
2022-12-26 18:29:15 +0300

    Added CarsDao interface

--- src/main/java/web/dao/CarDao.java
+++ src/main/java/web/dao/CarDao.java
@@ -1,27 +1,9 @@
 package web.dao;
 
-import org.springframework.stereotype.Component;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Component
-public class CarDao {
-    List<Car> carList = new ArrayList<>();
-
-    {
-        carList.add(new Car(1, "bmw 3 M competiton", 2018));
-        carList.add(new Car(2, "bmw 5 M competiton", 2021));
-        carList.add(new Car(3, "porsche panamera", 2022));
-        carList.add(new Car(4, "porsche 911", 2021));
-        carList.add(new Car(5, "Yahta", 2017));
-    }
-
-    public List<Car> getCars(int count) {
-        if (count >= 5) {
-            return carList;
-        }
-        return carList.subList(0, count);
-    }
+public interface CarDao {
+    public List<Car> getCars(int count);
 }
